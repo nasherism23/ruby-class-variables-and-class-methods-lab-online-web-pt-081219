6b064c6c0353bbd53a823dd7aa8ddc67ae30cfa4
James Nash <nash.jamesm@gmail.com>
2019-09-15 03:13:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,29 @@
 class Song 
-  
+attr_accessor :name, :artist, :genre 
+
+def initialize (name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@count +=1
+  @@genres << genre 
+  @@artists << artist
+end 
+
+@@count = 0
+@@genres = []
+@@artists = []
+ 
+  def self.count 
+    @@count 
+  end
+ 
+  def self.genres
+    @@genres.uniq 
+  end
+
+  def self.artists
+    @@artists.uniq
+  end 
+
+end 
