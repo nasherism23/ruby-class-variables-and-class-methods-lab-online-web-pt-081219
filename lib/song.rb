cd4c72aaa8b3e26c64180f6775b66f4c2e796eca
James Nash <nash.jamesm@gmail.com>
2019-09-15 03:19:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,17 @@ end
     @@genres.uniq 
   end
   
-  
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 
   def self.artists
     @@artists.uniq
